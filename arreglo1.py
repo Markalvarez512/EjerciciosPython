dfe2e69aa2f3070e0f1133344a92ab545bf3f55a
Marcelloalv512 <marcello.alvarez11803@est.uca.edu.ni>
2022-08-29 10:07:11 -0600

    primer ejercicio

---
+++ arreglo1.py
@@ -0,0 +1,15 @@
+numeros = []
+numeros.append (3)
+numeros.append (8)
+numeros.append (9)
+numeros.append (20)
+print ("Elementos del arreglo")
+print(numeros)
+
+#Para agregar un numero especifico en pocision del arreglo seria 
+numeros.insert(1, 15)
+print (numeros)
+
+#para remover un dato en especifico de un arreglo
+numeros.remove(3)
+print(numeros)
